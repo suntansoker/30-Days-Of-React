7c8b09051dec701e7e47c6b28edb5899bbc1abd6
Sayantan Sarkar <sayantanthepointer@gmail.com>
2020-10-15 20:39:37 +0530

    Updated-day09

--- execute.sh
+++ execute.sh
@@ -5,3 +5,7 @@ git push origin exercise-solutions
 git fetch upstream
 git merge upstream/master exercise-solutions
 git push origin exercise-solutions
+git checkout master
+git merge upstream/master master
+git push origin master
+git checkout exercise-solutions
